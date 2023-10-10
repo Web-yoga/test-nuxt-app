f89fbeed852a64f557541d0b3261b363f2d41361
Tsvetkov Alexander <hyehaeng@gmail.com>
2023-10-10 13:15:02 +0300

    test nuxt app

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="onClick">Products</button>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  methods: {
+    onClick() {
+      this.$router.push("/products");
+    },
+  },
+};
+</script>
---
+++ pages/products/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <nuxt-link to="/products/1">product 1</nuxt-link>
+    <nuxt-link to="/products/2">product 2</nuxt-link>
+  </div>
+</template>
