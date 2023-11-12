9b2a2555ecb53a701044375f90958a9f36c63d61
Alexander Tsvetkov <hyehaeng@gmail.com>
2023-11-12 15:58:18 +0300

    fetch posts for admin page

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -6,7 +6,7 @@
       >
     </section>
     <section class="existing-posts">
-      <PostList isAdmin />
+      <PostList isAdmin :posts="loadedPosts" />
     </section>
   </div>
 </template>
@@ -20,6 +20,11 @@ export default {
   components: {
     PostList,
     AppButton,
+  },
+  computed: {
+    loadedPosts() {
+      return this.$store.getters.loadedPosts;
+    },
   },
 };
 </script>
